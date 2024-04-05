9359bf9c3160f4c39bbb10ac732961374347545b
Ian Timmis <ianmtimmis@gmail.com>
2024-04-05 18:06:44 -0400

    Update wakey.py

--- wakey.py
+++ wakey.py
@@ -1,22 +1,40 @@
 '''
 Completely disables the ability for this mac to sleep while running. Upon 
-cancelling the command in terminal, sleeping becomes possible again. 
+cancelling the command in terminal, sleeping becomes possible again. Can also
+use the -on and -off flags to avoid leaving the command hanging.
 
 ** Must be ran with sudo to work properly. **
 '''
-
-import atexit
+import argparse
 import subprocess
+import signal
 
-def exit_handler():
-    subprocess.run('pmset disablesleep 0', shell=True)
+def set_sleep(disable):
+    cmd = 'pmset disablesleep {}'.format(int(disable))
+    subprocess.run(cmd, shell=True)
+    state = "disabled" if disable else "enabled"
+    print(f"Sleep has been {state}.")
+
+def exit_handler(signum, frame):
+    print("\nRestoring sleep settings...")
+    set_sleep(False)
+    exit(0)
 
 if __name__ == "__main__":
-    atexit.register(exit_handler)
+    parser = argparse.ArgumentParser(description='Control Mac Sleep Mode')
+    parser.add_argument('-on', action='store_true', help='Disable sleep mode')
+    parser.add_argument('-off', action='store_true', help='Enable sleep mode')
 
-    subprocess.run('pmset disablesleep 1', shell=True)
-
-    print("Your mac is holding its eyes open...")
-
-    while True:
-        pass
+    args, unknown = parser.parse_known_args()
+    
+    # Default behavior when no flags are provided
+    if not any(vars(args).values()):
+        signal.signal(signal.SIGINT, exit_handler)
+        set_sleep(True)
+        print("Your mac is holding its eyes open. Press Ctrl+C to allow it to sleep.")
+        signal.pause()
+    else:
+        if args.on:
+            set_sleep(True)
+        elif args.off:
+            set_sleep(False)
